031f8eefa68515719bf76ac97b8b7d8440b592fd
sqshq <sqshq@sqshq.com>
2019-03-03 14:35:29 -0500

    triggers config added

--- config/component.go
+++ config/component.go
@@ -13,8 +13,22 @@ type ComponentConfig struct {
 	Size          Size     `yaml:"size"`
 }
 
+type TriggerConfig struct {
+	Title     string  `yaml:"title"`
+	Condition string  `yaml:"condition"`
+	Action    *Action `yaml:"action,omitempty"`
+}
+
+type Action struct {
+	TerminalBell *bool `yaml:"terminal-bell,omitempty"`
+	Sound        *bool `yaml:"sound,omitempty"`
+	Visual       *bool `yaml:"visual,omitempty"`
+	Script       *bool `yaml:"script,omitempty"`
+}
+
 type GaugeConfig struct {
 	ComponentConfig `yaml:",inline"`
+	TriggerConfig   `yaml:",inline"`
 	Scale           *int              `yaml:"scale,omitempty"`
 	Color           *ui.Color         `yaml:"color,omitempty"`
 	Values          map[string]string `yaml:"values"`
@@ -23,18 +37,21 @@ type GaugeConfig struct {
 
 type BarChartConfig struct {
 	ComponentConfig `yaml:",inline"`
+	TriggerConfig   `yaml:",inline"`
 	Scale           *int        `yaml:"scale,omitempty"`
 	Items           []data.Item `yaml:"items"`
 }
 
 type AsciiBoxConfig struct {
 	ComponentConfig `yaml:",inline"`
+	TriggerConfig   `yaml:",inline"`
 	data.Item       `yaml:",inline"`
 	Font            *asciibox.AsciiFont `yaml:"font,omitempty"`
 }
 
 type RunChartConfig struct {
 	ComponentConfig `yaml:",inline"`
+	TriggerConfig   `yaml:",inline"`
 	Legend          *LegendConfig `yaml:"legend,omitempty"`
 	Scale           *int          `yaml:"scale,omitempty"`
 	Items           []data.Item   `yaml:"items"`
