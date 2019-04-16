09c16065f5e52786aa8504a0ea8e33a0ad82178e
sqshq <sqshq@sqshq.com>
2019-04-15 22:12:39 -0400

    fix possible index-out-of-range issue in palette

--- console/palette.go
+++ console/palette.go
@@ -67,9 +67,10 @@ func GetPalette(theme Theme) Palette {
 }
 
 func GetGradientColor(gradient []ui.Color, cur int, max int) ui.Color {
-	if cur > max {
-		cur = max
-	}
 	ratio := float64(len(gradient)) / float64(max)
-	return gradient[int(ratio*float64(cur))]
+	index := int(ratio * float64(cur))
+	if index > len(gradient)-1 {
+		index = len(gradient) - 1
+	}
+	return gradient[index]
 }
